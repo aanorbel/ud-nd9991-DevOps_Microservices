25bcabbea0dcff4e1853419f1fec7ed8d00fc13a
Norbel Ambanumben <aanorbel@gmail.com>
2020-07-18 14:54:19 +0100

    modified kubernetes run

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,11 @@ dockerpath=aanorbel/ml-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run ml-api-app --image=$dockerpath --port=8000
+kubectl run ml-api-app --image=$dockerpath --port=80 --labels app=ml-api-app
+
+
+# Wait to pod status will be ready
+kubectl wait pod/ml-api-app --for=condition=Ready --timeout=-1s
 
 # Step 3:
 # List kubernetes pods
@@ -17,3 +21,6 @@ kubectl get pods
 # Step 4:
 # Forward the container port to a host
 kubectl port-forward ml-api-app 8000:80
+
+# See the output of app running into pods
+kubectl logs project-microservices-udacity  
